37056b3b15d4cb69662656e9e2050701e5909d63
Iain Brighton <Iain Brighton>
2016-09-20 12:11:41 +0100

    Trims spaces in product display names

--- DSCResources/ROSSCommon/Src/Get-InstalledProductEntry.ps1
+++ DSCResources/ROSSCommon/Src/Get-InstalledProductEntry.ps1
@@ -3,68 +3,82 @@ function Get-InstalledProductEntry {
     .NOTES
         https://github.com/PowerShell/xPSDesiredStateConfiguration/blob/dev/DSCResources/MSFT_xPackageResource/MSFT_xPackageResource.psm1
 #>
+    [CmdletBinding()]
     param (
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $Name,
 
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $IdentifyingNumber,
 
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $InstalledCheckRegHive = 'LocalMachine',
 
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $InstalledCheckRegKey,
 
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $InstalledCheckRegValueName,
 
-        [Parameter()] [ValidateNotNullOrEmpty()]
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
         [System.String] $InstalledCheckRegValueData
     )
+    process {
 
-    $uninstallKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
-    $uninstallKeyWow64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall';
+        $uninstallKey = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall';
+        $uninstallKeyWow64 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall';
 
-    if ($IdentifyingNumber) {
-        $keyLocation = '{0}\{1}' -f $uninstallKey, $identifyingNumber;
-        $item = Get-Item $keyLocation -ErrorAction SilentlyContinue;
-        if (-not $item) {
-            $keyLocation = '{0}\{1}' -f $uninstallKeyWow64, $identifyingNumber;
+        if ($IdentifyingNumber) {
+            $keyLocation = '{0}\{1}' -f $uninstallKey, $identifyingNumber;
             $item = Get-Item $keyLocation -ErrorAction SilentlyContinue;
-        }
-        return $item;
-    }
-
-    foreach ($item in (Get-ChildItem -ErrorAction Ignore $uninstallKey, $uninstallKeyWow64)) {
-        if ($Name -eq (Get-LocalizableRegistryKeyValue $item 'DisplayName')) {
+            if (-not $item) {
+                $keyLocation = '{0}\{1}' -f $uninstallKeyWow64, $identifyingNumber;
+                $item = Get-Item $keyLocation -ErrorAction SilentlyContinue;
+            }
             return $item;
         }
-    }
 
-    if ($InstalledCheckRegKey -and $InstalledCheckRegValueName -and $InstalledCheckRegValueData) {
-        $installValue = $null;
-        $getRegistryValueIgnoreErrorParams = @{
-            RegistryHive = $InstalledCheckRegHive;
-            Key = $InstalledCheckRegKey;
-            Value = $InstalledCheckRegValueName;
-        }
-
-        #if 64bit OS, check 64bit registry view first
-        if ([System.Environment]::Is64BitOperatingSystem) {
-            $installValue = Get-RegistryValueIgnoreError @getRegistryValueIgnoreErrorParams -RegistryView [Microsoft.Win32.RegistryView]::Registry64;
-        }
-
-        if ($null -eq $installValue) {
-            $installValue = Get-RegistryValueIgnoreError @getRegistryValueIgnoreErrorParams -RegistryView [Microsoft.Win32.RegistryView]::Registry32;
-        }
-
-        if ($installValue) {
-            if ($InstalledCheckRegValueData -and $installValue -eq $InstalledCheckRegValueData) {
-                return @{ Installed = $true; }
+        foreach ($item in (Get-ChildItem -ErrorAction Ignore $uninstallKey, $uninstallKeyWow64)) {
+            $installedProduct = Get-LocalizableRegistryKeyValue -RegistryKey $item -ValueName 'DisplayName';
+            if ($installedProduct) {
+                $installedProduct = $installedProduct.Trim();
+            }
+            if ($Name -eq $installedProduct) {
+                return $item;
             }
         }
-    }
 
-    return $null;
+        if ($InstalledCheckRegKey -and $InstalledCheckRegValueName -and $InstalledCheckRegValueData) {
+            $installValue = $null;
+            $getRegistryValueIgnoreErrorParams = @{
+                RegistryHive = $InstalledCheckRegHive;
+                Key = $InstalledCheckRegKey;
+                Value = $InstalledCheckRegValueName;
+            }
+
+            #if 64bit OS, check 64bit registry view first
+            if ([System.Environment]::Is64BitOperatingSystem) {
+                $installValue = Get-RegistryValueIgnoreError @getRegistryValueIgnoreErrorParams -RegistryView [Microsoft.Win32.RegistryView]::Registry64;
+            }
+
+            if ($null -eq $installValue) {
+                $installValue = Get-RegistryValueIgnoreError @getRegistryValueIgnoreErrorParams -RegistryView [Microsoft.Win32.RegistryView]::Registry32;
+            }
+
+            if ($installValue) {
+                if ($InstalledCheckRegValueData -and $installValue -eq $InstalledCheckRegValueData) {
+                    return @{ Installed = $true; }
+                }
+            }
+        }
+
+        return $null;
+
+    } #end process
 } #end function Get-InstalledProductEntry
--- DSCResources/ROSSCommon/Src/Get-LocalizableRegistryKeyValue.ps1
+++ DSCResources/ROSSCommon/Src/Get-LocalizableRegistryKeyValue.ps1
@@ -3,18 +3,21 @@ function Get-LocalizableRegistryKeyValue {
     .NOTES
         https://github.com/PowerShell/xPSDesiredStateConfiguration/blob/dev/DSCResources/MSFT_xPackageResource/MSFT_xPackageResource.psm1
 #>
-
+    [CmdletBinding()]
     param (
         [Parameter()]
-        [System.Object] $RegKey,
+        [System.Object] $RegistryKey,
 
         [Parameter()]
         [System.String] $ValueName
     )
+    process {
 
-    $res = $RegKey.GetValue("{0}_Localized" -f $ValueName);
-    if (-not $res) {
-        $res = $RegKey.GetValue($ValueName);
+        $result = $RegistryKey.GetValue("{0}_Localized" -f $ValueName);
+        if (-not $result) {
+            $result = $RegistryKey.GetValue($ValueName);
+        }
+        return $result;
+
     }
-    return $res;
 } #end function Get-LocalizableRegistryKeyValue
